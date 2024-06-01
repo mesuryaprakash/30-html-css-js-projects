078519c396840b0567d5fabbb43d2cab6c80fac6
theboysurya <thesuryasingh9@gmail.com>
2024-06-01 15:22:46 +0530

    Third completed

---
+++ Mini Calendar/script.js
@@ -0,0 +1,12 @@
+const monthName = document.getElementById("month-name");
+const dayName = document.getElementById("day-name");
+const dayNumber = document.getElementById("day-number");
+const year = document.getElementById("year");
+
+const date = new Date();
+// console.log(date.getMonth());
+monthName.innerText = date.toLocaleString("en",{month:"long"})
+dayName.innerText = date.toLocaleString("en",{weekday:"long"})
+dayNumber.innerText = date.getDate();
+year.innerText = date.getFullYear();
+// monthName.innerText
